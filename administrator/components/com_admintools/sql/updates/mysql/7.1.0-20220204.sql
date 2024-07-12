b0c68a08e67b94932062cb0296f9741fbcb5e363
Xavier DANO <xavier.dano@heolwenn.com>
2024-07-13 00:30:33 +0200

    Only Contentbuilder module

--- administrator/components/com_admintools/sql/updates/mysql/7.1.0-20220204.sql
+++
@@ -1,9 +0,0 @@
-/**
- * @package   admintools
- * @copyright Copyright (c)2010-2024 Nicholas K. Dionysopoulos / Akeeba Ltd
- * @license   GNU General Public License version 3, or later
- */
-
--- Change column types
-
--- Bad update, removed in 7.1.1
